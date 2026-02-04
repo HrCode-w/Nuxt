f7ebd7102423bfd4c4061b20d7d9149f35d75f14
RR <1276356403@qq.com>
2026-02-04 17:06:27 +0800

    1

--- app/app.vue
+++ app/app.vue
@@ -7,12 +7,9 @@
   </div>
 </template>
 <script setup lang="ts">
-const runtimeConfig = useRuntimeConfig()
-const appConfig = useAppConfig()
-import { Analytics } from '@vercel/analytics/nuxt'
-Analytics()
-console.log('App Config:', appConfig)
-console.log('Runtime Config:', runtimeConfig)
+const runtimeConfig = useRuntimeConfig();
+const appConfig = useAppConfig();
+import { Analytics } from "@vercel/analytics/nuxt";
 </script>
 <style>
 .page-enter-active,
@@ -25,26 +22,4 @@ console.log('Runtime Config:', runtimeConfig)
   opacity: 0;
   filter: blur(1rem);
 }
- /* .slide-left-enter-active,
-.slide-left-leave-active,
-.slide-right-enter-active,
-.slide-right-leave-active {
-  transition: all 0.2s;
-}
-.slide-left-enter-from {
-  opacity: 0;
-  transform: translate(50px, 0);
-}
-.slide-left-leave-to {
-  opacity: 0;
-  transform: translate(-50px, 0);
-}
-.slide-right-enter-from {
-  opacity: 0;
-  transform: translate(-50px, 0);
-}
-.slide-right-leave-to {
-  opacity: 0;
-  transform: translate(50px, 0);
-} */
 </style>
